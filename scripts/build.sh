50dce8e7b2f55909483cc2a5f6b9d499b9356d75
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2024-02-16 14:25:48 +0330

    feat: update protos and scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,12 +4,15 @@ working_dir=$(pwd)
 proto_dirs=($(pwd)/protos)  # ./protos/
 python_dir=($(pwd)/src)     # ./src/
 
-# Add execute permission to `create_dir.sh`
-chmod +x ./scripts/create_dir.sh
-
-# Create ./src/ directory
-./scripts/create_dir.sh src
-
+# Create ./src/ dirctory
+directory="src"
+if [ ! -d "$directory" ]; then
+    # Create the directory
+    mkdir -p "$directory"
+    echo "Directory '$directory' created successfully."
+else
+    echo "Directory '$directory' already exists."
+fi
 
 # Build *.proto files within the ./protos/ directory
 python3 -m grpc_tools.protoc\
@@ -17,30 +20,3 @@ python3 -m grpc_tools.protoc\
     --python_out=$python_dir\
     --grpc_python_out=$python_dir\
     $proto_dirs/*.proto     # ./protos/*.proto
-
-
-# Find subdirectories under ./protos/
-subdirectories=$(find "$proto_dirs" -mindepth 1 -maxdepth 1 -type d)
-
-# .*proto files within subdirectories
-for proto_dir in $subdirectories
-do  
-    src_dir="${proto_dir/protos/src}"
-
-    # Check if the directory is empty
-    if [ -z "$(ls -A "$proto_dir")" ]; then
-        echo "Directory $proto_dir is empty. Skipping..."
-        continue
-    fi
-
-    # Create corresponding subdirectories under ./src/
-    dir="${src_dir##*/}"
-    ./scripts/create_dir.sh "src/$dir"
-
-    python3 -m grpc_tools.protoc\
-        -I.\
-        --proto_path=$proto_dirs\
-        --python_out=$src_dir\
-        --grpc_python_out=$src_dir\
-        $proto_dir/*.proto  # ./protos/<subdir>/*.proto
-done
